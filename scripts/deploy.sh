6a3e0e8237a2788687766f992671661cb811d59a
Marcus Ramsden <marcus@bitsden.com>
2023-04-18 10:23:40 +0200

    Remove AWS infrastructure

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-aws s3 cp build/main.zip s3://lido-rewards-exporter-source/
-aws lambda update-function-code --function-name lido-rewards-exporter --s3-bucket lido-rewards-exporter-source --s3-key main.zip --publish
-
--- scripts/get-api-base-url.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-aws cloudformation describe-stacks --stack-name lido-rewards-exporter --query 'Stacks[0].Outputs[?OutputKey==`ApiGatewayUrl`] | [0].OutputValue' --output text
